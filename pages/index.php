0c14e3691cc16543782d47e39a3d27973f186567
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-09-30 15:01:35 +0200

    Update CSS

--- pages/index.php
+++ pages/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Ouiches</title>
-        <link rel="stylesheet" type="text/css" href="styles/common/master.css"/>
-    </head>
-    <body>
-
         <div class="page-title">
             <h1>Ouiches</h1>
             <p>Les répliques les plus classes du monde</p>
