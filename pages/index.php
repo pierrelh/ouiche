b0201f728930beff83f120db2756fb51abae7ff0
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-09-30 15:13:08 +0200

    Header position corrected

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,3 @@
-        <div class="page-title">
-            <h1>Ouiches</h1>
-            <p>Les répliques les plus classes du monde</p>
-            <p>Extraits de OSS 117 - Le Caire nid d'espion</p>
-        </div>
-
         <div class="page-body">
 
             <div class="words-list">
