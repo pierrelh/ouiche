9205f4eb22fc2d1e086b03e20c0b6b000850a1de
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-09-30 11:33:31 +0200

    Page structure and CSS file
    Création d'un dossier pages avec un fichier PHP correspondant à la structure de la page. Mise à jour du fichier CSS.

---
+++ pages/index.php
@@ -0,0 +1,33 @@
+<div class="page-title">
+    <h1>Ouiches</h1>
+    <p>Les répliques les plus classes du monde</p>
+    <p>Extraits de La Classe Américaine</p>
+</div>
+
+<div class="page-body">
+
+    <div class="word-box">
+        <p>Titre de la réplique</p>
+        <p>Réplique</p>
+        <div class="share-box">
+            <ul class="list">
+                <li>Twitter</li>
+                <li>Facebook</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="words-list">
+        <ul class="list">
+            <li>Mot</li>
+            <li>Mot</li>
+            <li>Mot</li>
+            <li>Mot</li>
+            <li>Mot</li>
+            <li>Mot</li>
+            <li>Mot</li>
+            <li>Mot</li>
+        </ul>
+    </div>
+
+</div>
