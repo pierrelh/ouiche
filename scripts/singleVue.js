86b2e67d0cdbe2fa698ced71eeba2902bdddc151
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-09-30 16:28:44 +0200

    fix singleVue js

--- scripts/singleVue.js
+++ scripts/singleVue.js
@@ -4,7 +4,7 @@ function singleVueUpdate(identifier) {
     }else {
         if (document.getElementById("SingleVue").classList.contains('hidden')) {
             //Show the Single Vue section if hidded
-            document.getElementById("SingleVue").remove('hidden');
+            document.getElementById("SingleVue").classList.remove('hidden');
         }
         var chosedWord = document.getElementById(identifier);
         var word = chosedWord.getAttribute('data-word_libelle');
