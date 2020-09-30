73509852e7d10c386bde90a6fa95cae4b7fad9a0
Nicolas Resmi <n.resmi@etu-webschoolfactory.fr>
2020-09-30 16:48:48 +0200

    function share v1

---
+++ pages/share.php
@@ -0,0 +1,20 @@
+<div class="share-box">
+    <ul class="list">
+        <li class="twitter">
+            <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=hi" target="_blank" id="twitter-button">
+                <img src="img/twitter.png" alt="Twitter">
+            </a>
+        </li>
+
+        <li class="facebook" id="facebook">
+            <a href="https://www.facebook.com/sharer/sharer.php?u=hi" target="_blank" id="facebook-button">
+                <img src="img/facebook.png" alt="Facebook">
+            </a>
+        </li>
+
+        <li class="link" onclick="copyUrl();">
+            <img src="img/url.png" alt="Link">
+            <textarea id="url-link" style="position:absolute; top:-500px;"></textarea>
+        </li>
+    </ul>
+</div>
