ff8a44f83ec92eb464b24a81c6025a9adb542cc4
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-09-30 13:43:37 +0200

    Page single

--- pages/index.php
+++ pages/index.php
@@ -6,20 +6,6 @@
 
 <div class="page-body">
 
-    <div class="word-box">
-        <div class="word-title">
-            <div class="audio-btn"></div>
-            <p>Titre de la réplique</p>
-        </div>
-        <p>Réplique</p>
-        <div class="share-box">
-            <ul class="list">
-                <li>Twitter</li>
-                <li>Facebook</li>
-            </ul>
-        </div>
-    </div>
-
     <div class="words-list">
         <ul class="list">
             <li>Mot</li>
@@ -27,3 +13,5 @@
     </div>
 
 </div>
+
+<script src="scripts/call.js"></script>
---
+++ pages/single.php
@@ -0,0 +1,17 @@
+<div class="word-box">
+
+    <div class="word-title">
+        <div class="audio-btn"></div>
+        <p>Titre de la réplique</p>
+    </div>
+
+    <p>Réplique</p>
+
+    <div class="share-box">
+        <ul class="list">
+            <li>Twitter</li>
+            <li>Facebook</li>
+        </ul>
+    </div>
+
+</div>
