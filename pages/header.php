477b0087f58c6e04c10fed02cbee9a1ecde86391
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-09-30 17:01:08 +0200

    CSS

--- pages/header.php
+++ pages/header.php
@@ -8,6 +8,8 @@
         <link rel="stylesheet" type="text/css" href="styles/common/single.css"/>
         <link rel="stylesheet" type="text/css" href="styles/common/index.css"/>
         <link href="https://fonts.googleapis.com/css2?family=Passion+One&family=Roboto&display=swap" rel="stylesheet">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     
     <body>
