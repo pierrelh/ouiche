c08d86c5294013401ae956876ba8ec4df8ba7359
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-10-01 14:29:15 +0200

    add routing url to dynamise player with keyword

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
   require __DIR__ . '/pages/header.php';
   require __DIR__ . '/pages/single.php';
   require __DIR__ . '/pages/index.php';
+
   //Routing and get the keyword from the url
   $keyword = $_SERVER['REQUEST_URI'];
-  if ($keyword != "" || $keyword != "/") {
+  if ($keyword != "" && $keyword != "/") {
     //If a keyword is in the url
+    //call js function
     echo  "<script type='text/javascript'>
             singleVueUpdateKeyword(".$keyword.");
           </script>";
