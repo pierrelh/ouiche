a594efc101fcc7a77d215a690d582a5b23a26919
Pierre L'Hopital <lhopital.pierre@gmail.com>
2020-10-01 14:25:51 +0200

    add routing url to dynamise player with keyword

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
   require __DIR__ . '/pages/header.php';
   require __DIR__ . '/pages/single.php';
   require __DIR__ . '/pages/index.php';
-
+  //Routing and get the keyword from the url
+  $keyword = $_SERVER['REQUEST_URI'];
+  if ($keyword != "" || $keyword != "/") {
+    //If a keyword is in the url
+    echo  "<script type='text/javascript'>
+            singleVueUpdateKeyword(".$keyword.");
+          </script>";
+    
+  }
   
 ?>
--- scripts/singleVue.js
+++ scripts/singleVue.js
@@ -1,11 +1,14 @@
+//Update the single vue with identifier (user action)
 function singleVueUpdate(identifier) {
     if (identifier === undefined) {
+        //If no identifier is passed
         alert("Une erreur s'est produite, veuillez réessayer")
     }else {
         if (document.getElementById("SingleVue").classList.contains('hidden')) {
             //Show the Single Vue section if hidded
             document.getElementById("SingleVue").classList.remove('hidden');
         }
+        //Set all the keywords informations on the single vue
         var chosedWord = document.getElementById(identifier);
         var word = chosedWord.getAttribute('data-word_libelle');
         var citation = chosedWord.getAttribute('data-word_citation');
@@ -14,4 +17,10 @@ function singleVueUpdate(identifier) {
         document.getElementById("RepliqueTitle").innerHTML = word;
         document.getElementById("AudioPlayer").src = url;
     }
+}
+
+//Update the single vue with keyword (routing)
+function singleVueUpdateKeyword(keyword) {
+    var element = document.querySelectorAll("[data-word_uuid='"+keyword+"']");
+    singleVueUpdate(element.id);
 }
