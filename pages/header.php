720febe3841fef225839a2cc7bf7d78e4f4f98af
PaulBreard <p_breard@etu-webschoolfactory.fr>
2020-10-01 14:50:46 +0200

    Font update

--- pages/header.php
+++ pages/header.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" type="text/css" href="styles/common/share.css"/>
         <link rel="stylesheet" type="text/css" href="styles/common/index.css"/>
         
-        <link href="https://fonts.googleapis.com/css2?family=Passion+One&family=Roboto&family=Cairo:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Passion+One&family=Roboto&family=Cairo:wght@400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="//cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css">
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
